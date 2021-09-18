38a980126b179d888f7007eec754e86055bf61cd
Albert_Safin <Albert_Safin@epam.com>
2021-09-18 15:49:32 +0300

    added navi bar

--- hm_3_page_object/base_objects/elements/header.py
+++ hm_3_page_object/base_objects/elements/header.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from hm_3_page_object.base_objects.elements.base_element import BaseElement
 from hm_3_page_object.base_objects.elements.navigation_bar import NavigationBar
 
@@ -6,5 +8,6 @@ class Header(BaseElement):
     """
     Header class
     """
-    naviagation_bar = NavigationBar("//ul[contains(@class, 'nav__links--products js-offcanvas-links')]", name='navi', by='xpath')
+    navigation_bar = NavigationBar("//ul[contains(@class, 'nav__links--products js-offcanvas-links')]",
+                                   name='navigation_bar', by=By.XPATH)
 
