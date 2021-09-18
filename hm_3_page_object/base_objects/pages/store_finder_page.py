f931c71a276498a14e05ee1c0de3a056e6509d61
Sergey Kuznetsov <s.p.kuznetsov@gmail.com>
2021-09-18 21:12:56 +0300

    Store finder page object added.

---
+++ hm_3_page_object/base_objects/pages/store_finder_page.py
@@ -0,0 +1,88 @@
+from selenium.webdriver.common.by import By
+
+from hm_3_page_object.base_objects.base_page import BasePage
+from hm_3_page_object.base_objects.elements.base_element import BaseElement
+from hm_3_page_object.base_objects.elements.clickable_element import ClickableElement
+from hm_3_page_object.base_objects.elements.input import Input
+from hm_3_page_object.base_objects.elements.list import List
+
+
+class StoreFinderPage(BasePage):
+    # Search input group
+    query_input = Input(by=By.ID, value='storelocator-query', name='Query field')
+    search_button = ClickableElement(by=By.CLASS_NAME, value='button.btn.btn-primary', name='Search button')
+    find_nearest_stores_button = ClickableElement(by=By.ID, value='findStoresNearMe', name='Find nearest stores button')
+
+    # Search results group
+    pager_from_top = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-from'])[1]",
+        name='Top pager \'from\' value',
+    )
+    pager_to_top = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-to'])[1]",
+        name='Top pager \'to\' value',
+    )
+    pager_all_top = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-all'])[1]",
+        name='Top pager \'all\' value',
+    )
+
+    # Top paging buttons
+    previous_button_top = ClickableElement(
+        by=By.XPATH,
+        value="(//button[@class='btn.btn-default.js-store-finder-pager-prev'])[1]",
+        name='Top Previous button',
+    )
+    next_button_top = ClickableElement(
+        by=By.XPATH,
+        value="(//button[@class='btn.btn-default.js-store-finder-pager-next'])[1]",
+        name='Top Next button',
+    )
+
+    stores_list = List(
+        by=By.CLASS_NAME,
+        value='ul.store__finder--navigation-list js-store-finder-navigation-list',
+        name='Stores list',
+    )
+
+    # Store details block
+    store_image = BaseElement(
+        by=By.CLASS_NAME,
+        value='div.store__finder--details-image.js-store-image',
+        name='Store image',
+    )
+    store_name = BaseElement(by=By.CLASS_NAME, value='div.info__name.js-store-name', name='Store name')
+    store_address = BaseElement(by=By.CLASS_NAME, value='div.info__address', name='Store address')
+    map = BaseElement(by=By.CLASS_NAME, value='div#store__finder--map.js-store-finder-map', name='Map')
+    store_openings = BaseElement(by=By.CLASS_NAME, value='dl.dl-horizontal.js-store-openings', name='Store openings')
+    store_features = List(by=By.CLASS_NAME, value='ul.js-store-features', name='Store features')
+    pager_from_bottom = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-from'])[2]",
+        name='Bottom pager \'from\' value',
+    )
+    pager_to_bottom = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-to'])[2]",
+        name='Bottom pager \'to\' value',
+    )
+    pager_all_bottom = BaseElement(
+        by=By.XPATH,
+        value="(//span[@class='js-store-finder-pager-item-all'])[2]",
+        name='Bottom pager \'all\' value',
+    )
+
+    # Bottom paging buttons
+    previous_button_bottom = ClickableElement(
+        by=By.XPATH,
+        value="(//button[@class='btn.btn-default.js-store-finder-pager-prev'])[2]",
+        name='Top Previous button',
+    )
+    next_button_bottom = ClickableElement(
+        by=By.XPATH,
+        value="(//button[@class='btn.btn-default.js-store-finder-pager-next'])[2]",
+        name='Top Next button',
+    )
