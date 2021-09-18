ca505ef41ed045cddd6c39878f8c879ffe8973f1
Ivan Ievlev <ivan_ievlev@epam.com>
2021-09-18 15:00:12 +0300

    add footer

--- hm_3_page_object/base_objects/base_page.py
+++ hm_3_page_object/base_objects/base_page.py
@@ -2,12 +2,14 @@ from hm_3_page_object.helpers.browser import Browser
 from hm_3_page_object.helpers.page_urls import UrlFactory
 from hm_3_page_object.base_objects.elements.header import Header
 from hm_3_page_object.base_objects.elements.sidebar import Sidebar
+from hm_3_page_object.base_objects.elements.footer import Footer
 from selenium.webdriver.common.by import By
 
 
 class BasePage:
     header = Header(by=By.CLASS_NAME, value='m-l8', name='header')
     sidebar = Sidebar(by=By.CLASS_NAME, value='sidebar-menu', name='Side bar')
+    footer = Footer(by=By.XPATH, value='//footer', name='footer')
 
     def __init__(self):
         self._url = UrlFactory.get_url(self)
