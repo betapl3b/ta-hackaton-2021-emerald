3e301de73e57010f579b5120a982b101c36f17c1
Albert_Safin <Albert_Safin@epam.com>
2021-09-19 02:24:52 +0300

    reporter fixes

--- hm_3_page_object/helpers/reporter.py
+++ hm_3_page_object/helpers/reporter.py
@@ -38,18 +38,20 @@ def prepare_html(path):
             for step_index, step in enumerate(test['steps']):
                 step_id = f's-{test_index}-{step_index}'
                 step_id_list.append(step_id)
-                steps_html += f'''<tr id="{step_id}" class="hidden"><td>{step_index}</td>
-    <td colspan="3" class="step-name">{step['name']}</td><td>{step['result']['duration']}</td><td>{step['result']['status']}</td>
-    </tr>'''
+                steps_html += f'<tr id="{step_id}" class="hidden"><td>{step_index}</td>' \
+                              f'<td colspan="3" class="step-name">{step["name"]}</td>' \
+                              f'<td>{step["result"]["duration"]}</td>' \
+                              f'<td>{step["result"]["status"]}</td></tr>'
                 summ_dur += int(step['result']['duration'])
                 test_status = 'failed' if step['result']['status'] != 'passed' else test_status
 
-            html += f'''<tr><td><button type="button" id="b{test_index}" aria-expanded="false"
-    onclick="toggle(this.id,'{str(",#".join(step_id_list))}');"><svg xmlns="\http://www.w3.org/2000/svg&quot;"
-    viewBox="0 0 80 80" focusable="false"><path d="M70.3 13.8L40 66.3 9.7 13.8z"></path></svg></button></td>
-    <td class="test-index">{test_index}</td><td class="test-feature">{feature['name']}</td>
-    <td class="test-name">{test['name']}</td><td class="test-duration">{summ_dur}</td>
-    <td class="test-status">{test_status}</td></tr>{steps_html}'''
+            html += f'<tr><td><button type="button" id="b{test_index}" aria-expanded="false"' \
+                    f'onclick="toggle(this.id,"{str(",#".join(step_id_list))}");">' \
+                    f'<svg xmlns="http://www.w3.org/2000/svg&quot;"viewBox="0 0 80 80" focusable="false">' \
+                    '<path d="M70.3 13.8L40 66.3 9.7 13.8z"></path></svg></button></td>' \
+                    f'<td class="test-index">{test_index}</td><td class="test-feature">{feature["name"]}</td>' \
+                    f'<td class="test-name">{test["name"]}</td><td class="test-duration">{summ_dur}</td>' \
+                    f'<td class="test-status">{test_status}</td></tr>{steps_html}'
             test_index += 1
 
     return html
