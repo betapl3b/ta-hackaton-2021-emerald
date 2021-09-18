f5774dfc60ddc0bb3b5da8281e79663c4b481b06
Albert_Safin <Albert_Safin@epam.com>
2021-09-18 15:49:32 +0300

    added navi bar

--- hm_3_page_object/base_objects/elements/navigation_bar.py
+++ hm_3_page_object/base_objects/elements/navigation_bar.py
@@ -5,18 +5,22 @@ from hm_3_page_object.base_objects.elements.list import List
 
 class NavigationBar(BaseElement):
     NAVI_ELEMENTS = "//ul[contains(@class, 'nav__links--products js-offcanvas-links')]/li/span/a"
-    NAVI_ELEMENT_TEMPLATE = NAVI_ELEMENTS + "[contains(text(), {title})]"
+    NAVI_ELEMENT_TEMPLATE = NAVI_ELEMENTS + "[contains(text(), '{title}')]"
+    "//ul[contains(@class, 'nav__links--products js-offcanvas-links')]/li/span/a[contains(text(), '{title}')]/../..//div[contains(@class, 'sub-navigation-section col-md-')]/div"
     MENU_LIST = NAVI_ELEMENT_TEMPLATE + "/../..//div[contains(@class, 'sub-navigation-section col-md-')]/div"
     BRANDS_LIST = "//span/a[contains(text(), 'Brands')]/../..//ul[@class='sub-navigation-list has-title']/li/a"
-    SUB_ELEMENT_LIST = "//span/a[contains(text(), '{menu}}')]/../..//div[@class='title']" \
+    SUB_ELEMENT_LIST = "//span/a[contains(text(), '{menu}')]/../..//div[@class='title']" \
                        "[contains(text(), '{submenu}')]/../ul[@class='sub-navigation-list has-title']/li/a"
+    SUB_ELEMENT_LIST_1 = "//span/a[contains(text(), '{menu}')]/../..//div[@class='title']" \
+                         "[contains(text(), '{submenu}')]/../ul[@class='sub-navigation-list has-title']/li/a"
     #
 
     _links = {}
 
     @property
     def rows(self):
-        return [title.text for title in self._element.find_elements_by_xpath(self.NAVI_ELEMENTS)]
+        return [title.get_attribute('textContent') for title in
+                self._element.find_elements_by_xpath(self.NAVI_ELEMENTS)]
 
     def goto(self, title):
         return self._element.find_element_by_xpath(self.NAVI_ELEMENT_TEMPLATE.format(title=title)).click()
@@ -33,14 +37,18 @@ class NavigationBar(BaseElement):
                 result.update({
                     link: {
                         'All brands': {
-                            elem.text: elem for elem in self._element.find_elements_by_xpath(self.BRANDS_LIST)
+                            elem.get_attribute('textContent'): elem for elem in
+                            self._element.find_elements_by_xpath(self.BRANDS_LIST)
                         }
                     }
                 })
             else:
+                result.update({link: {}})
                 for submenu in self._element.find_elements_by_xpath(self.MENU_LIST.format(title=link)):
-                    for subelem in self._element.find_elements_by_xpath(
-                            self.SUB_ELEMENT_LIST.format(menu=link, submenu=submenu)
+                    result[link].update({submenu.get_attribute('textContent'): {}})
+                    for subelem in submenu.find_elements_by_xpath(
+                            self.SUB_ELEMENT_LIST_1.format(menu=link, submenu=submenu.get_attribute('textContent'))
                     ):
-                        result[link][submenu.text].update({subelem.text: subelem})
+                        result[link][submenu.get_attribute('textContent')].update(
+                            {subelem.get_attribute('textContent'): subelem})
         return result
