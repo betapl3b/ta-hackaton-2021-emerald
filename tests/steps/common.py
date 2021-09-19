e2a5f9fbaf6abb80de0a799a631d9b2e40003d22
Sergey Kuznetsov <s.p.kuznetsov@gmail.com>
2021-09-19 10:34:12 +0300

    Changed sleep to 2 in stores_table_is_shown.

--- tests/steps/common.py
+++ tests/steps/common.py
@@ -65,7 +65,7 @@ def click_magnifier_button():
 
 @then("stores table is shown")
 def stores_table_is_shown():
-    sleep(1)
+    sleep(2)
     assert StoreFinderPage().stores_list.is_displayed(), "Stores list isn't shown."
 
 
